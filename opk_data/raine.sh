290213be34591e1a2f948ba2b95fda187e546806
goldmojo <goldmojo@free.fr>
2020-11-01 17:23:48 +0100

    Small opk content change

--- opk_data/raine.sh
+++ opk_data/raine.sh
@@ -7,4 +7,4 @@ cd `dirname $0`
 
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 
-exec ./raine -screenx 320 -screeny 240 -nogui -rp "$romPath" -nb ${romName%%.*} 2>&1 >/var/tmp/raine.log
+exec ./raine -screenx 320 -screeny 240 -nb -nogui -rp "$romPath" -nb ${romName%%.*} 2>&1 >/var/tmp/raine.log
