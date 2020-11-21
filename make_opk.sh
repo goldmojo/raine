61f7f09c21be3452506dbb5aa1c60a00ef3e72ad
goldmojo <goldmojo@free.fr>
2020-11-21 16:43:59 +0100

    Adjust opk data

--- make_opk.sh
+++ make_opk.sh
@@ -3,9 +3,9 @@ cp -Rf output/usr/share/games/raine/* opk_data
 cp -f output/usr/games/raine opk_data
 cp -f output/usr/share/pixmaps/raine.png opk_data
 
-# Remove beta libs
-rm -f opk_data/libGL.so.1_BETA
-rm -f opk_data/libmuparser.so.2_BETA
+# Copy libs
+cp -f /home/philippe/src/gl4es/lib/libGL.so.1 opk_data
+cp -f /home/philippe/src/muparser/lib/libmuparser.so.2 opk_data
 
 rm -f raine.opk
-mksquashfs opk_data raine.opk -all-root -noappend -no-exports -no-xattrs
+/usr/bin/mksquashfs opk_data raine.opk -all-root -noappend -no-exports -no-xattrs -force-uid 1000 -force-gid 100
